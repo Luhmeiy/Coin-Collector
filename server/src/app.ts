0e450a7f14d0180937173aba2b8c79dfc85d4ab4
Luhmeiy <Medeiros.luizhenrique@yahoo.com.br>
2023-04-28 13:38:43 -0300

    Prepare server for deployment

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,12 +3,14 @@ import { router } from "./routes";
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 // Config JSON and form data response
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(router);
 
-app.listen(3000, () => {
-	console.log(`Connected to port 3000`);
+app.listen(port, () => {
+	console.log(`Connected to port ${port}`);
 });
